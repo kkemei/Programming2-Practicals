b777cff3a909c6a60f23467c1f5ca65b22d446ca
kkemei <kipngetich.kemei@my.jcu.edu.au>
2017-08-08 10:59:15 +1000

    1

---
+++ Prac-1/HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by jc428707 on 31/07/17.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ Prac-1/Task-5/src/MovieQuote.java
@@ -0,0 +1,7 @@
+public class MovieQuote {
+    public static void main(String[] args)
+    {
+        System.out.println("'Hasta la vista, baby'");
+        System.out.println("-Arnold Schwarzenegger (Terminator 2: Judgment Day)");
+    }
+}
